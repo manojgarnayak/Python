46ce8b5084644c3aac27ce5bdb44f1cb241e588d
manojgarnayak <manojgarnayak07@gmail.com>
2023-09-24 09:47:38 +0530

    while loop programs

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-s = "manoj kumar garnayak"
-print(s.index('a',14))
+#s = "manoj kumar garnayak"
+#print(s.index('a',14))
+
+lst = [10,20,30,40]
+l = len(lst)
+newlst = [lst[0]]
+sum = 0
+a = 1
+
+while a < l-1:
+    sum = sum + lst[a]
+    a = a + 1
+
+newlst.append(sum)
+newlst.append(lst[-1])
+
+print(newlst)
